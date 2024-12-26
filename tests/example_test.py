2aa4cd09779e43784e1b0008f0fff3a3d8d6cd23
Torben <59419684+entorb@users.noreply.github.com>
2024-12-26 23:45:23 +0100

    Initial

---
+++ tests/example_test.py
@@ -0,0 +1,22 @@
+"""Unit Tests for example.py."""
+
+# ruff: noqa: S101 D103 PLR2004 INP001
+
+import sys
+from pathlib import Path
+
+# import pytest
+
+sys.path.insert(0, (Path(__file__).parent.parent / "src").as_posix())
+
+from example import calc_sum, read_file
+
+
+def test_calc_sum() -> None:
+    assert calc_sum(1, 2.2) == 3.2
+
+
+def test_read_file() -> None:
+    assert (
+        read_file(Path("LICENSE")) == "                    GNU GENERAL PUBLIC LICENSE\n"
+    )
