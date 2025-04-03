024f1d3895b1dae8f80ecaebe90dc24cc4b9959d
Torben <59419684+entorb@users.noreply.github.com>
2025-04-03 20:52:26 +0200

    minor

--- src/example.py
+++ src/example.py
@@ -1,7 +1,5 @@
 """
 Example Python script.
-
-for testing Ruff findings and solutions
 """
 
 import datetime as dt
@@ -16,19 +14,18 @@ def calc_sum(x: int, y: float) -> float:
     return x + y
 
 
-def print_date() -> None:
-    """Print the current date."""
-    # DTZ011 do not use dt.date.today()
-    print(dt.datetime.now(tz=TZ_DE).date())
-
-
 def read_file(filename: Path) -> str:
     """Read a file and return first line."""
     with filename.open(encoding="utf-8") as f:
-        return f.readline()
+        return f.readline().strip()
+
+
+def get_date() -> dt.date:
+    """Return the current date."""
+    return dt.datetime.now(tz=TZ_DE).date()
 
 
 if __name__ == "__main__":
-    print_date()
     print(calc_sum(1, 2.2))
     print(read_file(Path("LICENSE")))
+    print(get_date())
--- tests/example_test.py
+++ tests/example_test.py
@@ -5,18 +5,20 @@
 import sys
 from pathlib import Path
 
-# import pytest
-
 sys.path.insert(0, (Path(__file__).parent.parent / "src").as_posix())
 
-from example import calc_sum, read_file
+import datetime as dt
+
+from example import calc_sum, get_date, read_file
 
 
 def test_calc_sum() -> None:
-    assert calc_sum(1, 2.2) == 3.2
+    assert calc_sum(1, 2) == 3
 
 
 def test_read_file() -> None:
-    assert (
-        read_file(Path("LICENSE")) == "                    GNU GENERAL PUBLIC LICENSE\n"
-    )
+    assert read_file(Path("LICENSE")) == "GNU GENERAL PUBLIC LICENSE"
+
+
+def test_get_date() -> None:
+    assert get_date() >= dt.date(2023, 3, 14)
