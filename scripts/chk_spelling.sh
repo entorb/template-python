b2b766a9f05d34557cee4995972630429a06733e
Torben <59419684+entorb@users.noreply.github.com>
2026-08-17 08:38:44 +0200

    cspell

--- scripts/chk_spelling.sh
+++ scripts/chk_spelling.sh
@@ -6,7 +6,7 @@ cd "$(dirname "$0")/.."
 # trap 'rm -f "$out"' EXIT INT TERM
 
 rm -f cspell-words-missing.txt
-pnpm dlx cspell-cli@10.0.1 --cache --gitignore --unique --words-only . > cspell-words-missing.txt 2> /dev/null
+pnpm dlx cspell-cli@10.0.1 --unique --words-only . > cspell-words-missing.txt 2> /dev/null
 status=$?
 
 if [ $status -ne 0 ]; then
