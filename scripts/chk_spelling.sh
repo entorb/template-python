1025b5e84103c67ae89f2cd467be0e70c1c64704
Torben <59419684+entorb@users.noreply.github.com>
2026-08-17 13:19:43 +0200

    scripts/chk_spelling.sh

--- scripts/chk_spelling.sh
+++ scripts/chk_spelling.sh
@@ -2,8 +2,6 @@
 
 # ensure we are in the root dir
 cd "$(dirname "$0")/.."
-# out=$(mktemp)
-# trap 'rm -f "$out"' EXIT INT TERM
 
 rm -f cspell-words-missing.txt
 pnpm dlx cspell-cli@10.0.1 --unique --words-only . > cspell-words-missing.txt 2> /dev/null
@@ -11,6 +9,5 @@ status=$?
 
 if [ $status -ne 0 ]; then
     echo "Found unknown spellings, see cspell-words-missing.txt. Fix or transfer to cspell-words.txt"
-    head -n 100 "$out"
 fi
 exit $status
